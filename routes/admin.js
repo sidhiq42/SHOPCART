ef1ebcbf7f7ebe63eae6492be7ea96a4191b3b69
Sidhiq N <sidhiq4242@gmail.com>
2024-02-21 23:06:48 +0530

    try3

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,45 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render("index",{admin:true});
+  
+  res.render("admin/admin-home",{admin:true});
 });
 
+router.get('/view-product',(req,res)=>{
+  let products = [
+    {
+      productname: "Nike",
+      category: "Shoes",
+      quantity: 10,
+      prize: 2000,
+      description: "This shoes are the most rated shoes for ever",
+      image: "images/nike.jpg",
+    },
+    {
+      productname: "Adidas",
+      category: "shoes",
+      quantity: 10,
+      prize: 2000,
+      description: "this shoes are the most rated shoes ever",
+      image: "images/adidas.jpg",
+    },
+    {
+      productname: "Puma",
+      category: "shoes",
+      quantity: 10,
+      prize: 2000,
+      description: "this shoes are the most rated shoes ever",
+      image: "images/puma.jpg",
+    },
+    {
+      productname: "Snikker",
+      category: "shoes",
+      quantity: 10,
+      prize: 2000,
+      description: "this shoes are the most rated shoes ever",
+      image: "images/snikker.jpg",
+    },
+  ];
+  res.render("admin/view-product",{products,admin:true,})
+})
 module.exports = router;
